62907f5c355541fc527c30d875fb71aedb642942
chooker <chooker@PCAM-PC009>
2023-05-12 10:38:30 -0400

    Add project files.

---
+++ ExcelAddIn1/Ribbon1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ExcelAddIn1
+{
+    partial class QBRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public QBRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.QB = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.QB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.QB);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // QB
+            // 
+            this.QB.Items.Add(this.button1);
+            this.QB.Label = "QB";
+            this.QB.Name = "QB";
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // QBRibbon
+            // 
+            this.Name = "QBRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.QB.ResumeLayout(false);
+            this.QB.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup QB;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal QBRibbon Ribbon1
+        {
+            get { return this.GetRibbon<QBRibbon>(); }
+        }
+    }
+}
---
+++ ExcelAddIn1/Ribbon1.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Tools.Excel.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Interop.QBFC14;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace ExcelAddIn1
+{
+    public partial class QBRibbon
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Pressed!");
+            query_customer();
+        }
+
+        public void query_customer()
+        {
+            bool sessionBegun = false;
+            bool connectionOpen = false;
+            QBSessionManager sessionManager = null;
+
+            try
+            {
+                //Create the session Manager object
+                sessionManager = new QBSessionManager();
+
+                //Create the message set request object to hold our request
+                IMsgSetRequest requestMsgSet = sessionManager.CreateMsgSetRequest("US", 14, 0);
+                requestMsgSet.Attributes.OnError = ENRqOnError.roeContinue;
+
+                //Connect to QuickBooks and begin a session
+                sessionManager.OpenConnection("", "Sample Code from OSR");
+                connectionOpen = true;
+                sessionManager.BeginSession("", ENOpenMode.omDontCare);
+                sessionBegun = true;
+
+                BuildCustomerQueryRq(requestMsgSet);
+
+                //Send the request and get the response from QuickBooks
+                IMsgSetResponse responseMsgSet = sessionManager.DoRequests(requestMsgSet);
+
+                //End the session and close the connection to QuickBooks
+                sessionManager.EndSession();
+                sessionBegun = false;
+                sessionManager.CloseConnection();
+                connectionOpen = false;
+
+                WalkCustomerAddRs(responseMsgSet);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (sessionBegun)
+                {
+                    sessionManager.EndSession();
+                }
+                if (connectionOpen)
+                {
+                    sessionManager.CloseConnection();
+                }
+            }
+        }
+        void BuildCustomerQueryRq(IMsgSetRequest requestMsgSet)
+        {
+            ICustomerQuery CustomerQueryRq = requestMsgSet.AppendCustomerQueryRq();
+
+            //Set field value for Name
+            CustomerQueryRq.ORCustomerListQuery.CustomerListFilter.ORNameFilter.NameFilter.MatchCriterion.SetValue(ENMatchCriterion.mcEndsWith);
+
+            Worksheet worksheet = Globals.Factory.GetVstoObject(
+Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets["Standard Quote"]);
+
+            // five digit customer id
+            Regex regex = new Regex(@"\d{5}$");
+
+            string name = worksheet.Range["B11"].Value.ToString();
+            Match match = regex.Match(name);
+            name = match.Value;
+
+            MessageBox.Show("Customer ID: " + name);
+
+            CustomerQueryRq.ORCustomerListQuery.CustomerListFilter.ORNameFilter.NameFilter.Name.SetValue(name);
+        }
+
+
+
+
+        static void WalkCustomerAddRs(IMsgSetResponse responseMsgSet)
+        {
+
+            if (responseMsgSet == null) return;
+            IResponseList responseList = responseMsgSet.ResponseList;
+            if (responseList == null) return;
+            //if we sent only one request, there is only one response, we'll walk the list for this sample
+            for (int i = 0; i < responseList.Count; i++)
+            {
+                IResponse response = responseList.GetAt(i);
+                //check the status code of the response, 0=ok, >0 is warning
+                if (response.StatusCode >= 0)
+                {
+                    //the request-specific response is in the details, make sure we have some
+                    if (response.Detail != null)
+                    {
+                        //make sure the response is the type we're expecting
+                        ENResponseType responseType = (ENResponseType)response.Type.GetValue();
+                        if (responseType == ENResponseType.rtCustomerQueryRs)
+                        {
+                            //upcast to more specific type here, this is safe because we checked with response.Type check above
+                            ICustomerRetList CustomerRet = (ICustomerRetList)response.Detail;
+                            //WalkCustomerRet(CustomerRet);
+                            MessageBox.Show($"Found {CustomerRet.GetAt(i).Name.GetValue()} as a customer");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

ca8c58b1150aff9bf5c6c3aa4b0ebab3111a41b4
Colton Hooker <chooker@bendtooling.com>
2023-06-21 13:52:14 -0400

    Removed RequestLibrary and made it into its own solution

--- ExcelAddIn1/QBUtility.cs
+++ ExcelAddIn1/QBUtility.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
-using RequestLibrary;
+using QBRequestLibrary;
 
 namespace ExcelAddIn1
 {
